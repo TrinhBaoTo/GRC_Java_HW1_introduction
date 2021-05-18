3a25e620c8eeac8adc4c7169b8d366e28676a758
Trinh Bao To <71686961+TrinhBaoTo@users.noreply.github.com>
2021-05-18 13:19:46 +0700

    Add files via upload

---
+++ HW1.java
@@ -0,0 +1,22 @@
+// Trinh To, CS141, Fall 2019, Section A
+// Programming Assignment #1, 09/30/19
+//
+// This program's behavior is displays the following information:
+   // My Official name
+   // My prefered name to be called.
+   // My current favorite food
+   // My current favorite past time.
+ 
+public class HW1
+{
+	public static void main(String[] args)
+   {
+	   System.out.println("/////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+      System.out.println("// Trinh Bao To                \\\\");
+      System.out.println("// Trinh                       \\\\");
+      System.out.println("// Korean BBQ                  \\\\");
+      System.out.println("// Hanging out with friends    \\\\");
+     	System.out.println("/////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+   } 
+}
+
